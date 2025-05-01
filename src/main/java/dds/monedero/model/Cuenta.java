ff64d74863ceea748271eacd24ddb4346733426b
ailenolha13 <ailenolha@gmail.com>
2025-04-30 22:13:19 -0300

    code smells commented

--- src/main/java/dds/monedero/model/Cuenta.java
+++ src/main/java/dds/monedero/model/Cuenta.java
@@ -10,10 +10,10 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Cuenta {
-
   private double saldo = 0;
   private List<Movimiento> movimientos = new ArrayList<>();
 
+  // Code smell 1 - se esta duplicando el saldo = 0
   public Cuenta() {
     saldo = 0;
   }
@@ -22,33 +22,41 @@ public class Cuenta {
     saldo = montoInicial;
   }
 
+  // Code smell 2 - el nombre del parametro, "cuanto", es poco expresivo
   public void poner(double cuanto) {
+    // Code smell 3 - se podria usar un try-catch y mejorar el manejo de errores y ademas ser mas
+    // expresivos y declarativos con la responsabilidad del metodo
+    // Code smell 4 - los mensajes hardcodeados de las excepcion deberian estar en la implementacion de la excepcion
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
-
+    // Code smell 5 - la implementacion de la validacion de la cantidad de movimientos podria estar en otro lado
     if (getMovimientos().stream()
         .filter(movimiento -> movimiento.fueDepositado(LocalDate.now()))
         .count() >= 3) {
       throw new MaximaCantidadDepositosException("Ya excedio los " + 3 + " depositos diarios");
     }
 
+    // Code smell 6 - no se esta utilizando el metodo para agregar el movimiento
     new Movimiento(LocalDate.now(), cuanto, true).agregateA(this);
   }
 
   public void sacar(double cuanto) {
+    // Code smell 7 - sucede lo mismo que code smell 3, 4
     if (cuanto <= 0) {
       throw new MontoNegativoException(cuanto + ": el monto a ingresar debe ser un valor positivo");
     }
     if (getSaldo() - cuanto < 0) {
       throw new SaldoMenorException("No puede sacar mas de " + getSaldo() + " $");
     }
+    // Code smell 8 - sucede lo mismo que code smell 5
     var montoExtraidoHoy = getMontoExtraidoA(LocalDate.now());
     var limite = 1000 - montoExtraidoHoy;
     if (cuanto > limite) {
       throw new MaximoExtraccionDiarioException(
           "No puede extraer mas de $ " + 1000 + " diarios, " + "límite: " + limite);
     }
+    // Code smell 9 - sucede lo mismo que code smell 6
     new Movimiento(LocalDate.now(), cuanto, false).agregateA(this);
   }
 
@@ -57,6 +65,7 @@ public class Cuenta {
     movimientos.add(movimiento);
   }
 
+  // Code smell 10 - el nombre del metodo podria ser mas expresivo
   public double getMontoExtraidoA(LocalDate fecha) {
     return getMovimientos().stream()
         .filter(movimiento -> !movimiento.isDeposito() && movimiento.getFecha().equals(fecha))
@@ -65,9 +74,12 @@ public class Cuenta {
   }
 
   public List<Movimiento> getMovimientos() {
+    // Code smell 11 - se deberia devolver una copia y no la referencia de la lista de movimientos,
+    // ya que sino se podria modificar desde afuera de la clase lo cual romperia el enccapsulamiento
     return movimientos;
   }
 
+  // Code smell 12 - no se deberia poder setear de afuera de la clase los movimientos
   public void setMovimientos(List<Movimiento> movimientos) {
     this.movimientos = movimientos;
   }
@@ -76,6 +88,7 @@ public class Cuenta {
     return saldo;
   }
 
+  // Code smell 13 - no se deberia poder setear de afuera de la clase el saldo
   public void setSaldo(double saldo) {
     this.saldo = saldo;
   }
--- src/main/java/dds/monedero/model/Movimiento.java
+++ src/main/java/dds/monedero/model/Movimiento.java
@@ -43,12 +43,14 @@ public class Movimiento {
   public boolean isExtraccion() {
     return !esDeposito;
   }
-
+  // Code smell 14 - Modificar el saldo o agregar el movimiento a la cuenta deberia ser responsabilidad de la clase Cuenta
   public void agregateA(Cuenta cuenta) {
     cuenta.setSaldo(calcularValor(cuenta));
     cuenta.agregarMovimiento(fecha, monto, esDeposito);
   }
-
+  // Code smell 16 - El nombre del metodo es poco expresivo y no comunica
+  // si esta calculando un nuevo saldo o simplemente el valor del movimiento
+  // Code smell 17 - acoplamiento con cuenta
   public double calcularValor(Cuenta cuenta) {
     if (esDeposito) {
       return cuenta.getSaldo() + getMonto();
